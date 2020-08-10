b016d056ea5c6f98c861d24f3de13b5dc205a234
Elias Martinez <jemart218@gmail.com>
2020-08-09 22:52:47 -0700

    spelling corrections, favicon feature added, map loads over AZ now, and home and about links scroll down the current tab.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function myMap() {
   var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
+    center:new google.maps.LatLng(35,-110),
     zoom:5,
   };
   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
